83b3d34aaeaac14c77d40fbeca4b0d339e9d20a3
Vinícius Santos <vinisantoosk@gmail.com>
2024-10-20 13:39:18 -0300

    feat: add styled components ao Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { FormularioHero, HeroTitle } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <FormularioHero>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      </HeroTitle>
     </div>
-  </form>
+  </FormularioHero>
 )
 
 export default Hero
